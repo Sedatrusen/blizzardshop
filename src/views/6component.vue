4a20f3778a435b17ee10f7de0ce01618cbfccda9
Sedatrusen <56399517+Sedatrusen@users.noreply.github.com>
2020-11-10 01:24:33 +0300

    Update 6component.vue

--- src/views/6component.vue
+++ src/views/6component.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <h1>Recommended</h1>
     <kutular />
   
   </div>
@@ -15,6 +16,16 @@ export default {
 </script>
 <style  scoped>
 * {
+  display: block;
     background-color :#002650;
+}
+h1{
+ text-align: start;
+margin:auto;
+margin-left: 3rem;
+    
+    font-weight: 500;
+    line-height: 1.2;
+    
 }
 </style>
