e269b5e49e7ac128f5e3fb84b1abd6a990cd80ae
Sedatrusen <56399517+Sedatrusen@users.noreply.github.com>
2020-11-10 01:04:23 +0300

    Update menu.vue

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -55,7 +55,7 @@
                 d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
               />
             </svg>
-            <input  type=" text" placeholder="Search Shop" />      </div>
+            <input class="input"  type=" text" placeholder="Search Shop" />      </div>
         </li>
           <div id="whish">
         <li >
@@ -131,7 +131,7 @@ ul{
 
 }
 li{
-  margin:1rem;
+  margin:10px;
   font-size: 1rem;
 }
 .svg{
@@ -141,20 +141,24 @@ li{
     color:#0e86ca
 }
 .arama{
-  margin-left: 20rem;
+  margin-left: 28rem;
   width: 30rem;
   border:1px solid rgba(255,255,255,.15);
   height:2rem ;
   display: flex;
+  padding:auto;
+  background-color: #001732;
 }
 input{
- color: red;
-  outline: none;
+  height: 100%;
+  width: 100%;
+   outline: none;
   border: none;
-  
+  color: white;
+ 
 }
-input:focus{
-  height: 2rem;
+.arama:focus{
+  height: 3rem;
 }
 .svgkucuk{
 width: 10px;
@@ -175,4 +179,5 @@ border:1px solid rgba(255,255,255,.15);
     height: 20px;
     color:#0e86ca
 }
+
 </style>
